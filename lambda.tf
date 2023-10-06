137263bcf6817722bc110bbe4e3166260f447d4a
Taylor Monacelli <taylormonacelli@gmail.com>
2023-10-06 10:15:51 -0700

    Python 3.11 runtime is available, try it out

--- lambda.tf
+++ lambda.tf
@@ -3,6 +3,6 @@ resource "aws_lambda_function" "terraform_lambda_func" {
   function_name = "Spacelift_Test_Lambda_Function"
   role          = aws_iam_role.lambda_role.arn
   handler       = "index.lambda_handler"
-  runtime       = "python3.8"
+  runtime       = "python3.11"
   depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
 }
